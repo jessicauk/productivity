41df94d93bdc67dd75f96ec3b295d3e90c9cb13a
Jessica Apolinar <jessica_220691@hotmail.com>
2024-01-15 00:19:22 -0600

    fix code

--- src/app/api/tasks/[taskId]/route.ts
+++ src/app/api/tasks/[taskId]/route.ts
@@ -1,11 +1,20 @@
 import { NextResponse } from "next/server";
 
-export function GET({ params }: { params: { taskId: string } }): NextResponse {
+export function GET(
+  request: any,
+  { params }: { params: { taskId: string } }
+): NextResponse {
   return NextResponse.json("GET" + params.taskId);
 }
-export function PUT({ params }: { params: { taskId: string } }): NextResponse {
+export function PUT(
+  request: any,
+  { params }: { params: { taskId: string } }
+): NextResponse {
   return NextResponse.json("PUT" + params.taskId);
 }
-export function DELETE({ params }: { params: { taskId: string } }): NextResponse {
+export function DELETE(
+  request: any,
+  { params }: { params: { taskId: string } }
+): NextResponse {
   return NextResponse.json("DELETE" + params.taskId);
 }
